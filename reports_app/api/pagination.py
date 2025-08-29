361f0c2b8b9bd7ae286d11c914b1d88b12d59ab1
Lewybundi <lewymbuba@gmail.com>
2025-08-29 07:08:58 +0300

    testing

--- reports_app/api/pagination.py
+++ reports_app/api/pagination.py
@@ -1,6 +1,6 @@
 from rest_framework.pagination import PageNumberPagination
 
 class ScamPostListPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 20
     page_query_param = 'P'
     
